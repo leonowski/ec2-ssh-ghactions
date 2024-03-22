0d37d5e198604da7f4bdd13c6213dfcb0d67c164
Noel Ferreria <nferreria@ameripharma.us>
2024-03-22 12:47:05 -0700

    make things simpler and readable

--- ec2-ssh.sh
+++ ec2-ssh.sh
@@ -3,8 +3,14 @@ ec2-user=${{ inputs.username }}
 command=${{ inputs.command }}
 ec2-hostname=${{ inputs.ec2_instance_id }}
 
-# Use Proxy command to connect to the EC2 instance
-result=$(ssh -o ProxyCommand="sh -c "aws sso login; aws ec2-instance-connect send-ssh-public-key --instance-id $ec2-hostname --instance-os-user $ec2-user --ssh-public-key "file://$HOME/.ssh/id_rsa.pub"; aws ssm start-session --target $ec2-hostname --document-name AWS-StartSSHSession --parameters 'portNumber=22'"" "$command")
+# Generate temporary keypair
+ssh-keygen -t rsa -b 4096 -f $HOME/.ssh/id_rsa
+
+# Send the public key to the EC2 instance
+aws ec2-instance-connect send-ssh-public-key --instance-id $ec2-hostname --instance-os-user $ec2-user --region $AWS_REGION --ssh-public-key "file://$HOME/.ssh/id_rsa.pub"
+
+# SSH command to execute
+result=$(ssh -i $HOME/.ssh/id_rsa $ec2-user@$ec2-hostname $command)
 
 
 # Set the output value
