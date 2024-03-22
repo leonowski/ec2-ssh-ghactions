f356e88f6be5cf32ebd2026bf80313e96204a62c
Noel Ferreria <nferreria@ameripharma.us>
2024-03-22 14:28:35 -0700

    learning about composite actions

--- ec2-ssh.sh
+++ ec2-ssh.sh
@@ -1,18 +1,13 @@
 #!/usr/bin/env bash
-# Read the input values
-ec2-user=${{ inputs.username }}
-command=${{ inputs.command }}
-ec2-hostname=${{ inputs.ec2_instance_id }}
 
 # Generate temporary keypair
 ssh-keygen -t rsa -b 4096 -f $HOME/.ssh/id_rsa -N ""
 
 # Send the public key to the EC2 instance
-aws ec2-instance-connect send-ssh-public-key --instance-id ${ec2-hostname} --instance-os-user ${ec2-user} --region $AWS_REGION --ssh-public-key "file://$HOME/.ssh/id_rsa.pub"
+aws ec2-instance-connect send-ssh-public-key --instance-id ${EC2_INSTANCE_ID} --instance-os-user ${USERNAME} --region ${AWS_REGION} --ssh-public-key "file://$HOME/.ssh/id_rsa.pub"
 
 # SSH command to execute
-result=$(ssh -i $HOME/.ssh/id_rsa $ec2-user@$ec2-hostname $command)
-
+result=$(ssh -i $HOME/.ssh/id_rsa $ec2-user@$ec2-hostname ${COMMAND})
 
 # Set the output value
-echo "command_output=$result" >> $GITHUB_OUTPUT
+echo "command_output=$result" >>$GITHUB_OUTPUT
