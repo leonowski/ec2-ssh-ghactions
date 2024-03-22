48bd1472d85263e13a1e99fbbbb300ccb13c038b
Noel Ferreria <nferreria@ameripharma.us>
2024-03-22 14:48:57 -0700

    add -t option to ssh to capture output of remote command

--- ec2-ssh.sh
+++ ec2-ssh.sh
@@ -9,7 +9,7 @@ ssh-keygen -t rsa -b 4096 -f $HOME/.ssh/id_rsa -N ""
 aws ec2-instance-connect send-ssh-public-key --instance-id ${EC2_INSTANCE_ID} --instance-os-user ${USERNAME} --region ${AWS_REGION} --ssh-public-key "file://$HOME/.ssh/id_rsa.pub"
 
 # SSH command to execute
-result=$(ssh -o 'ProxyCommand aws ssm start-session --target ${EC2_INSTANCE_ID} --document-name AWS-StartSSHSession --parameters 'portNumber=22'' -o StrictHostKeyChecking=no ${USERNAME}@${EC2_PUBLIC_IP} "${COMMAND}")
+result=$(ssh -t -o 'ProxyCommand aws ssm start-session --target ${EC2_INSTANCE_ID} --document-name AWS-StartSSHSession --parameters 'portNumber=22'' -o StrictHostKeyChecking=no ${USERNAME}@${EC2_PUBLIC_IP} "${COMMAND}")
 
 # Set the output value
 echo "command_output=$result" >>$GITHUB_OUTPUT
