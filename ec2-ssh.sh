41e01ac45336124f819db2b7d147ba3ab66d7e1e
Noel Ferreria <nferreria@ameripharma.us>
2024-03-22 14:11:25 -0700

    fixes

--- ec2-ssh.sh
+++ ec2-ssh.sh
@@ -5,10 +5,10 @@ command=${{ inputs.command }}
 ec2-hostname=${{ inputs.ec2_instance_id }}
 
 # Generate temporary keypair
-ssh-keygen -t rsa -b 4096 -f $HOME/.ssh/id_rsa
+ssh-keygen -t rsa -b 4096 -f $HOME/.ssh/id_rsa -N ""
 
 # Send the public key to the EC2 instance
-aws ec2-instance-connect send-ssh-public-key --instance-id $ec2-hostname --instance-os-user $ec2-user --region $AWS_REGION --ssh-public-key "file://$HOME/.ssh/id_rsa.pub"
+aws ec2-instance-connect send-ssh-public-key --instance-id ${ec2-hostname} --instance-os-user ${ec2-user} --region $AWS_REGION --ssh-public-key "file://$HOME/.ssh/id_rsa.pub"
 
 # SSH command to execute
 result=$(ssh -i $HOME/.ssh/id_rsa $ec2-user@$ec2-hostname $command)
