243b8c85eca3ea92e702325215324c4fa40fdc1c
Noel Ferreria <nferreria@ameripharma.us>
2024-04-08 17:35:33 -0700

    remove double quotes around command

--- ec2-ssh.sh
+++ ec2-ssh.sh
@@ -9,7 +9,7 @@ ssh-keygen -t rsa -b 4096 -f $HOME/.ssh/id_rsa -N ""
 aws ec2-instance-connect send-ssh-public-key --instance-id ${EC2_INSTANCE_ID} --instance-os-user ${USERNAME} --region ${AWS_REGION} --ssh-public-key "file://$HOME/.ssh/id_rsa.pub"
 
 # SSH command to execute
-result=$(ssh -tt -o 'ProxyCommand aws ssm start-session --target ${EC2_INSTANCE_ID} --document-name AWS-StartSSHSession --parameters 'portNumber=22'' -o StrictHostKeyChecking=no ${USERNAME}@${EC2_INSTANCE_ID} "\"${COMMAND}\"")
+result=$(ssh -tt -o 'ProxyCommand aws ssm start-session --target ${EC2_INSTANCE_ID} --document-name AWS-StartSSHSession --parameters 'portNumber=22'' -o StrictHostKeyChecking=no ${USERNAME}@${EC2_INSTANCE_ID} ${COMMAND})
 
 # Set the output value
 echo "command_output<<EOF" >>$GITHUB_OUTPUT
