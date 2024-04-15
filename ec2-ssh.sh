8976321bfd04bc945841d4d00c2af54b615ac9cd
Noel Ferreria <nferreria@ameripharma.us>
2024-04-15 10:21:19 -0700

    revert recent changes

--- ec2-ssh.sh
+++ ec2-ssh.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -xe
+set -e
 
 # Generate temporary keypair
 ssh-keygen -t rsa -b 4096 -f $HOME/.ssh/id_rsa -N ""
@@ -9,9 +9,9 @@ ssh-keygen -t rsa -b 4096 -f $HOME/.ssh/id_rsa -N ""
 aws ec2-instance-connect send-ssh-public-key --instance-id ${EC2_INSTANCE_ID} --instance-os-user ${USERNAME} --region ${AWS_REGION} --ssh-public-key "file://$HOME/.ssh/id_rsa.pub"
 
 # SSH command to execute
-result=$(ssh -tt -o 'ProxyCommand aws ssm start-session --target ${EC2_INSTANCE_ID} --document-name AWS-StartSSHSession --parameters 'portNumber=22'' -o StrictHostKeyChecking=no ${USERNAME}@${EC2_INSTANCE_ID} ${COMMAND})
+result=$(ssh -tt -o 'ProxyCommand aws ssm start-session --target ${EC2_INSTANCE_ID} --document-name AWS-StartSSHSession --parameters 'portNumber=22'' -o StrictHostKeyChecking=no ${USERNAME}@${EC2_INSTANCE_ID} "${COMMAND})"
 
 # Set the output value
-echo "command_output<<EOF" >>$GITHUB_OUTPUT
+echo "command_output<<EOF" >>$GITHUB_OUTPUTt
 echo "$result" >>$GITHUB_OUTPUT
 echo "EOF" >>$GITHUB_OUTPUT
